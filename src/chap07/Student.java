e84100088754ec00507b8048d9d4fcd96fd1d6ba
qufnr <qufnr@icloud.com>
2021-05-13 16:35:04 +0900

    2021 05 13 다형성

--- src/chap07/Student.java
+++ src/chap07/Student.java
@@ -5,7 +5,11 @@ public class Student extends People {
 	public int studentNo;
 	
 	public Student(String name, String ssn, int studentNo) {
-		super(name, ssn);
+//		자식 클래스의 생성자에서 부모 클래스의 생성자를 호출함
+//		컴파일러는 부모클래스의 기본 생성자를 호출하는 super() 만 자동으로 생성함
+//		보무 클래스에 기본 생성자를 작성하여 처리하거나 자식 클래스의 생성자에서 부모클래스의 생성자를 호출할 수 있도록 매개변수가 있는 super(...) 를 호출해야 함
+		
+		super(name,ssn);
 		this.studentNo = studentNo;
 	}
 
